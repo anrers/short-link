1f4d6130d70448388a52fc8fb5847fd0b87290eb
Vital Pyzhyk <pyzhykvv@gmail.com>
2025-04-07 21:13:57 +0300

    feat: Add the ability to redirect from short link

--- src/app/Domain/Link/Providers/ActionsServiceProvider.php
+++ src/app/Domain/Link/Providers/ActionsServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Domain\Link\Providers;
 
 use App\Domain\Link\Actions\CreateLinkAction;
+use App\Domain\Link\Actions\GetLinkByCodeAction;
 use App\Domain\Link\Contracts\Actions\CreateLinkContract;
+use App\Domain\Link\Contracts\Actions\GetLinkByCodeContract;
 use Illuminate\Support\ServiceProvider;
 
 class ActionsServiceProvider extends ServiceProvider
@@ -11,6 +13,7 @@ class ActionsServiceProvider extends ServiceProvider
     public $bindings
         = [
             CreateLinkContract::class => CreateLinkAction::class,
+            GetLinkByCodeContract::class => GetLinkByCodeAction::class,
         ];
 
     public function register(): void
